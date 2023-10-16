4124884d973f7e5e5e10eae8e939f8e0ab95f615
RyanCan'tCode <mryane714@gmail.com>
2023-10-15 23:30:10 -0500

    finished

--- Classwork/Programming_Projects/Classwork/Programming_Projects/PP5/PasswordManager.java
+++ Classwork/Programming_Projects/Classwork/Programming_Projects/PP5/PasswordManager.java
@@ -1,44 +1,113 @@
 package Classwork.Programming_Projects.PP5;
-
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class PasswordManager {
     public static void main(String[] args) {
         // Declare Variables
-        ArrayList<User> users = new ArrayList<>();
         String userName;
-        String Enteredpassword;
-        StringBuilder password;
         int option;
         Scanner in = new Scanner(System.in);
+        ArrayList<User> users = new ArrayList<>();
+        boolean foundUser = false;
 
+        // main menu
         do {
             System.out.println("===== Login =====");
-            System.out.println("[0] Login ");
-            System.out.println("[1] Create account");
-            System.out.println("[2] Exit\n");
+            System.out.println("[0] Add User");
+            System.out.println("[1] Change User Password");
+            System.out.println("[2] Check Password Strength");
+            System.out.println("[3] Display Users");
+            System.out.println("[4] Remove User");
+            System.out.println("[5] Exit\n");
 
             option = in.nextInt();
-
-            if(option == 1) {
-                System.out.println("Enter a username: ");
-                userName = in.next();
-
-                System.out.println("Enter a password: ");
-                Enteredpassword = in.next();
-                password = new StringBuilder(Enteredpassword);
-                
-
-                }
-
-
             
+            // user options
+            switch(option) {
+                case 0:
+                    users.add(createUser());
+                    break;
+            
+                case 1:
+                    do {
+                        
+                        System.out.println("Enter a username to change the password: ");
+                        userName = in.next();
+                        for(User user : users) {
+                            if(user.getUserName().equals(userName)) {
+                                user.changePassword();
+                                foundUser = true;
+                            }
+                        }
 
-            if(option == 2) {
+                    } while(foundUser != true);
+                    break;
+                
+                    case 2:
+                        do {
+                        
+                        System.out.println("Enter a username to check password strength: ");
+                        userName = in.next();
+                        for(User user : users) {
+                            if(user.getUserName().equals(userName)) {
+                                user.checkPasswordStrength(user.getPassWord());
+                                foundUser = true;
+                            }
+                        }
+
+                    } while(foundUser != true);
+                    break;
+
+                    case 3:
+                        for(User user : users) {
+                            System.out.println(user.toString());
+                        }
+                        break;
+                    
+                    case 4:
+                        do {
+                        
+                            System.out.println("Enter a username to REMOVE USER: ");
+                            userName = in.next();
+                            
+                            Iterator<User> iterator = users.iterator();
+                            while (iterator.hasNext()) {
+                                User user = iterator.next();
+                                if (user.getUserName().equals(userName)) {
+                                    iterator.remove();
+                                    System.out.println("User removed successfully.");
+                                    foundUser = true;
+                                    break;
+                                }
+                            }
+
+                    } while(foundUser != true);
+                    break;
+
+                    case 5:
+                        continue;
+                        
+                    default:
+                        System.out.println("Invalid input");
+
 
             }
-        } while(option != 2);
+        } while(option != 5);
+    }
+
+    // function that creates a user
+    public static User createUser() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter user name: ");
+        String userName = in.next();
+
+        System.out.println("Enter password: ");
+        String initialPassword = in.next();
+        StringBuilder password = new StringBuilder(initialPassword);
+        return new User(userName, password);
     }
 
 }
--- Classwork/Programming_Projects/Classwork/Programming_Projects/PP5/User.java
+++ Classwork/Programming_Projects/Classwork/Programming_Projects/PP5/User.java
@@ -1,17 +1,237 @@
+/*
+ * @Author: Ryan Eubanks
+ * @Date: 10/15/23
+ * 
+ * Problem Statement:
+ * Create a program that can manage users by creating them, checking their password strength, removing them, and changing their password
+ */
 package Classwork.Programming_Projects.PP5;
+import java.util.Scanner;
+import java.util.Random;
 
 public class User {
     private String userName;
     private StringBuilder password;
+    private int passLength;
+    private int option;
+    private static int shift = 3;
+    Scanner in = new Scanner(System.in);
+    Random random = new Random();
     
+    // constructor
+    public User(String userName) {
+        this.userName = userName;
+        this.password = changePassword();
+
+    }
+    // alternate constructor
     public User(String userName, StringBuilder password) {
         this.userName = userName;
         this.password = password;
-
+    }
+    
+    @Override
+    public String toString() {
+        return this.userName + " : " + this.password;
     }
 
-    public void changePassword(StringBuilder newPass) {
-        this.password = newPass;
+    // function that gives different password changing options
+    public StringBuilder changePassword() {
+        boolean condition = false;
+        do {
+            System.out.println("===== Password Creation =====");
+            System.out.println("[0] Manual password entry \n");
+            System.out.println("Generate password ");
+            System.out.println("[1] Lowercase letters");
+            System.out.println("[2] Uppercase letters");
+            System.out.println("[3] Numbers");
+            System.out.println("[4] Numbers and lowercase letters");
+            System.out.println("[5] Numbers and Uppercase letters");
+            System.out.println("[6] Number and a mix of Upper and lowercase letters");
+            option = in.nextInt();
+
+            switch(option) {
+                case 0:
+                    manualChange();
+                    condition = true;
+                    break;
+                case 1:
+                    passwordLength();
+                    lowerLettersOnly();
+                    condition = true;
+                    break;
+                case 2:
+                    passwordLength();
+                    upperCaseLetters();
+                    condition = true;
+                    break;
+                case 3:
+                    passwordLength();
+                    numbers();
+                    condition = true;
+                    break;
+                case 4:
+                    passwordLength();
+                    nums_LowerLetters();
+                    condition = true;
+                    break;
+                case 5:
+                    passwordLength();
+                    nums_UpperLetters();
+                    condition = true;
+                    break;
+                case 6:
+                    passwordLength();
+                    nums_Upper_LowerLetters();
+                    condition = true;
+                    break;
+                default: 
+                    System.out.println("Invalid input");
+
+            }
+        } while(condition != true);
+
+        return this.password;
+    }
+
+    // ===================== Getters ===========================
+    public String getUserName() {
+        return this.userName;
+    }
+
+    public StringBuilder getPassWord() {
+        return this.password;
+    }
+
+    // =================== Aux Funcitons =======================
+
+    public void checkPasswordStrength(StringBuilder userPassword) {
+        String password = userPassword.toString();
+
+        int score = 0;
+
+        boolean hasUppercase = password.matches(".*[A-Z].*");
+        boolean hasLowercase = password.matches(".*[a-z].*");
+        boolean hasSpecialCharacter = password.matches(".*[^a-zA-Z].*");
+        boolean hasMinimumLength = password.length() >= 8;
+
+        if (hasUppercase) {
+            score++;
+        }
+        if (hasLowercase) {
+            score++;
+        }
+        if (hasSpecialCharacter) {
+            score++;
+        }
+        if (hasMinimumLength) {
+            score++;
+        }
+
+        if (score == 4) {
+            System.out.println("Strong password");
+        } 
+        else if (score >= 2) {
+            System.out.println("Moderate password");
+        } 
+        else {
+            System.out.println("Your password sucks!");
+        }
+    }
+    public int passwordLength() {
+        System.out.println("Enter the amount characters your password will have: ");
+        this.passLength = in.nextInt();
+        return this.passLength;
+    }
+
+    public void manualChange() {
+        System.out.println("Enter a password: ");
+        this.password = new StringBuilder(in.next());
+    }
+
+    //** Auto Password Generationg Options
+    public void lowerLettersOnly() {
+        StringBuilder newPassword = new StringBuilder();
+        for (int j = 0; j <= passLength; j++) {
+            int ASCIIletters = random.nextInt(26);
+            char lowerCaseletters = (char) (ASCIIletters + 97);
+            newPassword.append(lowerCaseletters);
+        }
+        this.password = newPassword;
+    }
+
+    public void upperCaseLetters() {
+        StringBuilder newPassword = new StringBuilder();
+        for (int i = 0; i <= passLength; i++) {
+            int ASCIIBigletters = random.nextInt(26);
+            char UpperCaseletters = (char) (ASCIIBigletters + 65);
+            newPassword.append(UpperCaseletters);
+        }
+        this.password = newPassword;
+    }
+
+    public void numbers() {
+        StringBuilder newPassword = new StringBuilder();
+        for (int i = 0; i <= passLength; i++) {
+            int ASCIInumbers = random.nextInt(10);
+            char numbers = (char) (ASCIInumbers + 48);
+            newPassword.append(numbers);
+        }
+        this.password = newPassword;
+    }
+
+    public void nums_LowerLetters() {
+        StringBuilder newPassword = new StringBuilder();
+        for (int i = 0; i <= passLength; i++) {
+            int ASCIInumbers = random.nextInt(10);
+            char numbers = (char) (ASCIInumbers + 48);
+            int ASCIIlowerletters = random.nextInt(26);
+            char lowerLetters = (char) (ASCIIlowerletters + 97);
+            newPassword.append(numbers + "" + lowerLetters);
+        }
+        this.password = newPassword;
+    }
+    
+    public void nums_UpperLetters() {
+        StringBuilder newPassword = new StringBuilder();
+        for (int j = 0; j <= passLength; j++) {
+            int ASCIInumbers = random.nextInt(10);
+            char numbers = (char) (ASCIInumbers + 48);
+            int ASCIIBigletters = random.nextInt(26);
+            char UpperCaseletters = (char) (ASCIIBigletters + 65);
+            newPassword.append(numbers + "" + UpperCaseletters);
+        }
+        this.password = newPassword;
+    }
+
+    public void nums_Upper_LowerLetters() {
+        StringBuilder newPassword = new StringBuilder();
+        for (int i = 0; i <= passLength; i++) {
+            int ASCIInumbers = random.nextInt(10);
+            char numbers = (char) (ASCIInumbers + 48);
+            int ASCIIBigletters = random.nextInt(26);
+            char UpperCaseletters = (char) (ASCIIBigletters + 65);
+            int ASCIIlowerletters = random.nextInt(26);
+            char lowerLetters = (char) (ASCIIlowerletters + 97);
+            newPassword.append(numbers + "" + UpperCaseletters + "" + lowerLetters);
+        } 
+        this.password = newPassword;      
+    }
+
+    // maybe implement later
+    public void encrypt(StringBuilder password, int shift) {
+        for (int i = 0; i < password.length(); i++) {
+            char originalChar = password.charAt(i);
+            char encryptedChar = (char) (originalChar + shift);
+            password.setCharAt(i, encryptedChar);
+        }
+    }
+    public void decrypt(StringBuilder password, int shift) {
+        for (int i = 0; i < password.length(); i++) {
+            char encryptedChar = password.charAt(i);
+            char decryptedChar = (char) (encryptedChar - shift);
+            password.setCharAt(i, decryptedChar);
+        }
     }
 
 }
