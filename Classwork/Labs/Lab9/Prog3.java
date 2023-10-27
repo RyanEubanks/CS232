4ae271d1380599f62f3c38600fe825c1bef773bf
RyanCan'tCode <74330824+RyanbadProgrrammer@users.noreply.github.com>
2023-10-27 13:32:05 -0500

    more lab9 upadtes

--- Classwork/Labs/Lab9/Prog3.java
+++ Classwork/Labs/Lab9/Prog3.java
@@ -3,6 +3,7 @@ package Classwork.Labs.Lab9;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Prog3 {
@@ -13,6 +14,9 @@ public class Prog3 {
             Scanner sc = new Scanner(new File("Prog3.txt"));
             PrintStream ps = new PrintStream(new File("output.txt"));
 
+            ArrayList<Integer> numbers = new ArrayList<>();
+            // cannot use primitive type with arraylist
+
             // process each line of the text from the file
             while(sc.hasNextLine()) {
                 String line = sc.nextLine(); // 8 9 7 3
@@ -20,9 +24,16 @@ public class Prog3 {
                 int product = 1;
                 while(lineScanner.hasNextInt()) {
                     int number = lineScanner.nextInt();
+                    numbers.add(number);
                     product *= number;
                 }
-                ps.println(product);
+                System.out.println(numbers);
+                int sum = 0
+                for(int num : numbers) {
+                    sum += num;
+                }
+                
+                ps.println("Product: " + product  + " " + "Sum: " + sum);
             }
             
         }
